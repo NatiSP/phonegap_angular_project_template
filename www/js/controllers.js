cca622e82c7f7d79a49891e89d5ec964b6d02de2
Daniel Dihardja <daniel.dihardja@socian.net>
2013-11-11 11:20:08 +0100

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,396 @@
+'use strict';
+
+/* Controllers */
+var mod = angular.module('orderApp.controllers', []);
+
+function AppModel() {
+	this.data = {
+		path : "",
+		menu : [],
+		location : {},
+		config : {}
+	};
+	
+	this.cache = function() {
+		
+	}
+}
+
+function OrderModel() {
+	
+	var _this = this;
+	
+	this.data = {
+		items : [],
+		status : 0,
+		total : 0,
+		orderid: ''
+	}
+	
+	this.clear = function() {
+		_this.data.items = [];
+		_this.data.status = 0;
+		_this.data.total = 0;
+		_this.data.orderid = '';		
+	}
+	
+	this.countTotal = function() {
+		var total = 0;
+		for(var i=0; i<_this.data.items.length; i++) {
+			var item = _this.data.items[i];
+			total += Number(item.price);
+		}
+		_this.data.total = total;
+	}
+}
+
+
+
+mod.factory('appModel', function() {
+	return new AppModel();
+});
+
+mod.factory('orderModel', function() {
+	return new OrderModel();
+});
+
+mod.factory('ws', function() {
+	return new AutobahnWebSocket();
+}); 
+
+mod.controller('NavigationController', function($scope, $location) {
+	$scope.onClickScan = function() {
+		if(! confirm('Möchten Sie die Seite verlassen?')) return;
+		$location.path('/scan');	
+	}
+	
+	$scope.onClickSettings = function() {
+		alert('Settings');
+	}
+	
+	$scope.onClickOrder = function() {
+		alert('orders');
+	}
+});
+
+// TODO:
+// outsource this into the configuration file
+var orderMode;
+
+mod.controller('StartController', function($scope, $location, appModel) {
+	appModel.data.path = "herbertzstuttgart";
+	$scope.onStartSingleMode = function() {
+		orderMode = "single";
+		appModel.data.path = "herbertzstuttgart";
+		$location.path('/initialize');
+	}
+	
+	$scope.onStartMultiMode = function() {
+		orderMode = "multi";
+		appModel.data.path = "herbertzstuttgart";
+		$location.path('/initialize');
+	}
+	
+	$scope.scanLocation = function() {
+		
+		// dev shortcut
+		appModel.data.path = "herbertzstuttgart";
+		$location.path('/initialize');
+		return;
+		
+		cordova.plugins.barcodeScanner.scan(function(result) {
+			if(result.cancelled) return;
+			var qrObj = JSON.parse(result.text);
+			appModel.data.path = qrObj.path; 
+			$scope.$apply(function() {
+				$location.path('/initialize');
+			});
+
+		}, function(error) {
+			alert("Scanning failed: " + error);
+		});
+	}
+	
+});
+
+mod.controller('InitializeController', function($scope, $http, $location, appModel, ws) {
+	
+	var _this = this;
+	$scope.status = "";
+	this.loadAppData = function() {
+		$scope.status = "Laden ...";
+		
+		var dataPath = "http://socian.net/" + appModel.data.path + "/data.json?r=" + Math.random(1);
+		
+		$http.get( dataPath ).success(function(data) {
+			appModel.data.location = data.location;
+			appModel.data.menu = data.menu;
+			appModel.data.config = data.config;
+			
+			_this.checkInternetConnection();
+		});	
+	}
+	
+	// if the wifiCheck is set true check the internet connection
+	this.checkInternetConnection = function() {
+		$scope.status = "Prüfe WI-FI ...";
+		if(appModel.data.config['wificheck'] == "true") {
+			// check the internet connection	
+			var networkState = navigator.network.connection.type;
+			if(networkState != Connection.WIFI) {
+				alert('Sie müssen mit dem lokalen WI-FI verbunden sein');
+				$location.path('/start');
+			}
+			else _this.createWSConnection();
+		}
+		else _this.createWSConnection();
+	}
+	
+	this.createWSConnection = function() {
+		$scope.status = "Prüfe Web Socket ...";
+		ws.onopen = function() {
+			$scope.$apply(function() {
+				if(orderMode == "single") $location.path('/menuorder');
+				else if(orderMode == "multi") $location.path('/multiorder');	
+			})
+		}
+		
+		ws.onerror= function(err) {
+			alert(err);
+			$scope.$apply(function() {
+				$location.path('/start');
+			});
+		}
+		
+		var host = appModel.data.config['wshost'];
+		ws.connect(host);
+	}
+	
+	// start the controller
+	this.loadAppData();
+});
+
+
+//-------------------------------------------------------------//
+//      Commands for communicating with the order server
+//-------------------------------------------------------------//
+
+// TODO:
+// outsource this in an external file that can be included
+// by the server, patron and staff 
+var OrderCommand = {
+	REGISTER_STAFF : "registerStaff",
+	NEW_ORDER : "newOrder",
+	GET_ORDER : "getOrder",
+	GET_ORDER_LIST : "getOrderList",
+	UPDATE_ORDER_STATUS_READY : "updateOrderStatusReady",
+	UPDATE_ORDER_STATUS_COMPLETE : "updateOrderStatusComplete",
+	ORDER_UPDATE : "orderUpdate"
+}
+
+mod.controller('OrderController', function($scope, $http, $location, appModel, orderModel, ws) {
+	
+	var _this = this;
+	
+		
+	orderModel.clear();
+	$scope.orderTotal = orderModel.data.total;
+	$scope.orderStatus = orderModel.data.status;
+	
+	$scope.menu = appModel.data.menu;
+	$scope.locationName = appModel.data.location.name;
+	$scope.locationAddress = appModel.data.location.address;
+	
+	$scope.onAddItem = function(index) {
+		var item = appModel.data.menu[index];
+		orderModel.data.items.push(item);
+		orderModel.countTotal();
+		
+		$scope.items = orderModel.data.items;
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onRemoveItem = function(index) {
+		orderModel.data.items.splice(index,1);
+		$scope.items = orderModel.data.items;
+		
+		orderModel.countTotal();
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onClearOrder = function() {
+		if(! confirm('Do you want to clear your order?')) return;
+		orderModel.clear();
+		orderModel.countTotal();
+		$scope.items = orderModel.data.items;
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onSubmitOrder = function() {
+		var msg = "";
+		for(var i=0; i<orderModel.data.items.length; i++) {
+			var item = orderModel.data.items[i];
+			msg += item.name + " " + item.price + " EUR\n";
+		}
+		msg += "\nTotal: " + orderModel.data.total + " EUR";
+		
+		if(! confirm( msg )) return;
+		
+		var msg = {command:OrderCommand.NEW_ORDER, data:orderModel.data }
+		ws.send( JSON.stringify(msg) );
+	}
+	
+	$scope.onRescanLocation = function() {
+		$location.path('/scan');
+	}
+	
+	//--------------------------//
+	//    web socket section
+	//--------------------------//
+	
+	this.handler = {}
+	this.handler[ OrderCommand.ORDER_UPDATE ] = function(data) {
+		if(data == null) {
+				_this.storage.removeItem('order_id');
+				return;
+			}
+			orderModel.data = data;
+			$scope.$apply(function() {
+				
+				$scope.items = orderModel.data.items;
+				$scope.orderTotal = orderModel.data.total;
+				$scope.orderStatus = parseInt(orderModel.data.status);	
+				$scope.orderId = orderModel.data.orderid;
+				
+				if($scope.orderStatus == 2) {
+					alert('Iher Bestellung kann abgeholt werden');
+				}
+				
+				// store the order id into a cookie
+				_this.storage.setItem('order_id', orderModel.data.orderid);
+			});		
+	}
+
+	ws.onmessage = function(message) {
+		var msg = angular.fromJson(message);
+		var handler = _this.handler[msg.command];
+		if(handler != null) {
+			handler.apply(handler, [msg.data]);
+		}
+	}
+	
+	ws.onerror = function(err) {
+		alert("WSERROR: " + err);
+		$scope.$apply(function() {
+			$location.path('/scan');
+		});
+	}
+	
+	// check if we have an open order
+	// by looking for the existing order id
+	this.storage = window.localStorage;
+	var oid = this.storage.getItem('order_id');
+	
+	if(oid != null) {
+		var msg = {command:OrderCommand.GET_ORDER,data:{orderid:oid}}
+		ws.send(JSON.stringify(msg));
+	}
+});
+
+
+//-------------------------------------------------------------//
+//      Controller for handling multiple orders
+//-------------------------------------------------------------//
+
+mod.controller('MultiOrderController', function($scope, $http, $location, appModel, orderModel, ws) {
+	
+	var _this = this;
+	
+		
+	orderModel.clear();
+	$scope.orderTotal = orderModel.data.total;
+	$scope.orderStatus = orderModel.data.status;
+	
+	$scope.menu = appModel.data.menu;
+	$scope.locationName = appModel.data.location.name;
+	$scope.locationAddress = appModel.data.location.address;
+	
+	$scope.onAddItem = function(index) {
+		var item = appModel.data.menu[index];
+		orderModel.data.items.push(item);
+		orderModel.countTotal();
+		
+		$scope.items = orderModel.data.items;
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onRemoveItem = function(index) {
+		orderModel.data.items.splice(index,1);
+		$scope.items = orderModel.data.items;
+		
+		orderModel.countTotal();
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onClearOrder = function() {
+		if(! confirm('Do you want to clear your order?')) return;
+		orderModel.clear();
+		orderModel.countTotal();
+		$scope.items = orderModel.data.items;
+		$scope.orderTotal = orderModel.data.total;
+	}
+	
+	$scope.onSubmitOrder = function() {
+		var msg = "";
+		for(var i=0; i<orderModel.data.items.length; i++) {
+			var item = orderModel.data.items[i];
+			msg += item.name + " " + item.price + " EUR\n";
+		}
+		msg += "\nTotal: " + orderModel.data.total + " EUR";
+		
+		if(! confirm( msg )) return;
+		
+		var msg = {command:OrderCommand.NEW_ORDER, data:orderModel.data }
+		ws.send( JSON.stringify(msg) );
+		
+		orderModel.clear();
+		$scope.items = orderModel.data.items;
+		$scope.orderTotal = orderModel.data.total;
+		$scope.orderStatus = parseInt(orderModel.data.status);	
+		$scope.orderId = orderModel.data.orderid;
+	}
+	
+	$scope.onRescanLocation = function() {
+		$location.path('/start');
+	}
+	
+	//--------------------------//
+	//    web socket section
+	//--------------------------//
+	
+	this.handler = {}
+	ws.onmessage = function(message) {
+		var msg = angular.fromJson(message);
+		var handler = _this.handler[msg.command];
+		if(handler != null) {
+			handler.apply(handler, [msg.data]);
+		}
+	}
+	
+	ws.onerror = function(err) {
+		alert("WSERROR: " + err);
+		$scope.$apply(function() {
+			$location.path('/start');
+		});
+	}
+	
+	// check if we have an open order
+	// by looking for the existing order id
+	this.storage = window.localStorage;
+	var oid = this.storage.getItem('order_id');
+	
+	if(oid != null) {
+		var msg = {command:OrderCommand.GET_ORDER,data:{orderid:oid}}
+		ws.send(JSON.stringify(msg));
+	}
+});
